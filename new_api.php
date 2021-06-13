ca65db2b4a94e2aacc7dfe89ee44c1ce4b880617
Laura959 <70938274+Laura959@users.noreply.github.com>
2021-06-14 00:48:02 +0300

    Update new_api.php

--- new_api.php
+++ new_api.php
@@ -11,6 +11,8 @@ $input = json_decode(file_get_contents('php://input'),true); //grazina array
 // connect to the mysql database
 $con = mysqli_connect($host, $user, $pass, $dbName);
 
+header("Access-Control-Allow-Origin"); //remove in production
+
 switch ($path) {
   case '/getContentList':
     // $sql = "SELECT news.ID, news.TITLE, news.CONTENT, news.CREATION, users.NAME, users.SURNAME, users.EMAIL, COUNT(likes.PERSON) as LIKES FROM news INNER JOIN users ON news.AUTHOR = users.ID INNER JOIN likes ON news.ID = likes.NEWS_ID";
@@ -153,6 +155,50 @@ case '/showComments':
       echo ']'; 
     }
     break;
+case '/newComment':
+    $sql = "INSERT INTO comments (CONTENT, NEWS_ID, AUTHOR) VALUES ('".$input['content']."', '".$input['id']."', (SELECT ID FROM users WHERE NAME = '".$input['username']."'))";
+    $statement = mysqli_query($con,$sql);
+  
+    if (!$statement) {
+      http_response_code(404);
+      die("error");
+    }
+    else {
+      header("Content-type:application/json");    
+          http_response_code(200);        
+  }
+  break;
+case '/searchNews':
+  $sql = "SELECT news.ID, news.TITLE, news.CONTENT, news.CREATION, users.NAME, users.SURNAME, users.EMAIL FROM news INNER JOIN users ON news.AUTHOR = users.ID WHERE users.NAME LIKE '%".$input['content']."%' OR news.CONTENT LIKE '%".$input['content']."%' 
+  ORDER BY news.ID DESC"; 
+  // $sql = "SELECT news.ID, news.TITLE, news.CONTENT, news.CREATION, users.NAME, users.SURNAME, users.EMAIL FROM news INNER JOIN users ON news.AUTHOR = users.ID";   
+      $statement = mysqli_query($con,$sql);
+  if (!$statement) {
+    http_response_code(404);
+    die("error");
+    }
+  else {
+    header("Content-type:application/json");
+    http_response_code(200);
+    echo '[';
+    $i=0;
+    $number = mysqli_num_rows ( $statement );
+    while($row = mysqli_fetch_assoc($statement)){
+      if($i == $number-1) {
+        $row['LIKES'] = countLikes($row['ID']);
+        $row['COMMENTS'] = countComments($row['ID']);
+        echo json_encode($row);
+      } else {
+        $row['LIKES'] = countLikes($row['ID']);
+        $row['COMMENTS'] = countComments($row['ID']);
+        echo json_encode($row);
+        echo ',';
+      }
+      $i++;
+    }
+    echo ']'; 
+  }
+  break;
 }
 
 
