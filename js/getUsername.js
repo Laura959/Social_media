acd5bfa7939ea2e7058745cd38d9291ff5e5f65b
Laura959 <70938274+Laura959@users.noreply.github.com>
2021-06-16 00:14:57 +0300

    Update getUsername.js

--- js/getUsername.js
+++ js/getUsername.js
@@ -38,7 +38,8 @@
                     var postImage = document.createElement('img');
                     image.appendChild(postImage);
                     postImage.setAttribute('alt', 'img');
-                    postImage.setAttribute('src', '#');
+                    postImage.setAttribute('class', 'news-image');
+                    postImage.setAttribute('src', "data:image/gif;base64," + list[i].NEWS_IMAGE + "");
 
                     // Text Content
                     var textDiv = document.createElement('div');
@@ -49,14 +50,18 @@
                     var portfolioImg = document.createElement('img');
                     newsText.appendChild(portfolioImg);
                     portfolioImg.setAttribute('alt', 'portfolio');
-                    portfolioImg.setAttribute('src', '#');
+                    portfolioImg.setAttribute('class', 'portfolio-image');
+                    portfolioImg.setAttribute('src', "data:image/gif;base64," + list[i].IMAGE + "");
                     var infoDiv = document.createElement('div');
                     newsText.appendChild(infoDiv);
+                    infoDiv.setAttribute('class', 'info-div');
                     var h2 = document.createElement('h2');
+                    h2.setAttribute('class', 'h2-username');
                     infoDiv.appendChild(h2);
                     h2.setAttribute('id', 'username2');
                     h2.innerHTML = list[i].NAME;
                     var h3 = document.createElement('h3');
+                    h3.setAttribute('class', 'upload-time');
                     infoDiv.appendChild(h3);
                     h3.innerHTML = list[i].CREATION;
                     var p = document.createElement('p');
@@ -68,32 +73,67 @@
                     var buttons = document.createElement('div');
                     card.appendChild(buttons);
                     buttons.setAttribute('class', 'buttons');
+                    var likeDiv = document.createElement('div');
+                    buttons.appendChild(likeDiv);
+                    likeDiv.setAttribute('class', 'like-div');
                     var like = document.createElement('button');
-                    buttons.appendChild(like);
+                    likeDiv.appendChild(like);
                     like.setAttribute('class', 'like');
                     like.setAttribute('onclick', 'likeNews(this.id);'); // for FF
                     like.onclick = function() { likeNews(this.id); }; // for IE
                     like.setAttribute('id', 'like-' + list[i].ID);
-                    like.innerHTML = 'Like';
+                    like.setAttribute('type', 'submit');
+                    var likeIcon = document.createElement('i');
+                    like.appendChild(likeIcon);
+                    likeIcon.setAttribute('class', 'far fa-thumbs-up');
                     var span = document.createElement('span');
                     like.appendChild(span);
                     span.innerHTML = list[i].LIKES;
+                    var myLikes = document.createElement('div');
+                    likeDiv.appendChild(myLikes);
+                    myLikes.innerHTML = "Like";
+                    var editDiv = document.createElement('div');
+                    buttons.appendChild(editDiv);
+                    editDiv.setAttribute('class', 'edit-div');
                     var edit = document.createElement('button');
-                    buttons.appendChild(edit);
+                    editDiv.appendChild(edit);
                     edit.setAttribute('class', 'edit');
                     edit.setAttribute('onclick', 'editNews(this.id);'); // for FF
                     edit.onclick = function() { editNews(this.id); }; // for IE
                     edit.setAttribute('id', list[i].ID);
-                    edit.innerHTML = 'Edit';
+                    var editIcon = document.createElement('i');
+                    edit.appendChild(editIcon);
+                    editIcon.setAttribute('class', 'far fa-edit');
+                    var editTitle = document.createElement('div');
+                    editDiv.appendChild(editTitle);
+                    editTitle.innerHTML = "Edit";
+                    var deleteDiv = document.createElement('div');
+                    deleteDiv.setAttribute('class', 'delete-div');
+                    buttons.appendChild(deleteDiv);
                     var deleted = document.createElement('button');
-                    buttons.appendChild(deleted);
+                    deleteDiv.appendChild(deleted);
                     deleted.setAttribute('class', 'delete');
                     deleted.setAttribute('onclick', 'deleteNews(this.id);'); // for FF
                     deleted.onclick = function() { deleteNews(this.id); }; // for IE
                     deleted.setAttribute('id', 'delete-' + list[i].ID);
-                    deleted.innerHTML = 'Delete';
+                    var deleteIcon = document.createElement('i');
+                    deleted.appendChild(deleteIcon);
+                    deleteIcon.setAttribute('class', 'far fa-trash-alt');
+                    var deleteTitle = document.createElement('div');
+                    deleteDiv.appendChild(deleteTitle);
+                    deleteTitle.innerHTML = "Delete";
                     var inputDiv = document.createElement('div');
-                    buttons.appendChild(inputDiv);
+                    card.appendChild(inputDiv);
+                    var spanComment = document.createElement('span');
+                    inputDiv.appendChild(spanComment);
+                    var link = document.createElement('a');
+                    spanComment.appendChild(link);
+                    link.setAttribute('class', 'number-of-comments');
+                    link.setAttribute('href', '#');
+                    link.setAttribute('id', 'comment-' + list[i].ID);
+                    link.setAttribute('onclick', 'showComments(this.id);'); // for FF
+                    link.onclick = function() { showComments(this.id); }; // for IE
+                    link.innerHTML = list[i].COMMENTS + ' comments';
                     var comment = document.createElement('textarea');
                     inputDiv.appendChild(comment);
                     comment.setAttribute('rows', '1');
@@ -105,18 +145,10 @@
                     var comBtn = document.createElement('button');
                     inputDiv.appendChild(comBtn);
                     comBtn.innerHTML = "Submit";
+                    comBtn.setAttribute('class', 'comment-button');
                     comBtn.setAttribute('onclick', 'submitComment(this.id);'); // for FF
                     comBtn.onclick = function() { submitComment(this.id); }; // for IE
                     comBtn.setAttribute('id', 'btn-id-' + list[i].ID);
-                    var spanComment = document.createElement('span');
-                    inputDiv.appendChild(spanComment);
-                    var link = document.createElement('a');
-                    spanComment.appendChild(link);
-                    link.setAttribute('href', '#');
-                    link.setAttribute('id', 'comment-' + list[i].ID);
-                    link.setAttribute('onclick', 'showComments(this.id);'); // for FF
-                    link.onclick = function() { showComments(this.id); }; // for IE
-                    link.innerHTML = list[i].COMMENTS + ' comments';
                     i++;
                 }
             } else {
@@ -161,7 +193,8 @@
                 var postImage = document.createElement('img');
                 image.appendChild(postImage);
                 postImage.setAttribute('alt', 'img');
-                postImage.setAttribute('src', '#');
+                postImage.setAttribute('class', 'news-image');
+                postImage.setAttribute('src', "data:image/gif;base64," + list[i].NEWS_IMAGE + "");
 
                 // Text Content
                 var textDiv = document.createElement('div');
@@ -172,15 +205,19 @@
                 var portfolioImg = document.createElement('img');
                 newsText.appendChild(portfolioImg);
                 portfolioImg.setAttribute('alt', 'portfolio');
-                portfolioImg.setAttribute('src', '#');
+                portfolioImg.setAttribute('class', 'portfolio-image');
+                portfolioImg.setAttribute('src', "data:image/gif;base64," + list[i].IMAGE + "");
                 var infoDiv = document.createElement('div');
                 newsText.appendChild(infoDiv);
+                infoDiv.setAttribute('class', 'info-div');
                 var h2 = document.createElement('h2');
                 infoDiv.appendChild(h2);
                 h2.setAttribute('id', 'username2');
+                h2.setAttribute('class', 'h2-username');
                 h2.innerHTML = list[i].NAME;
                 var h3 = document.createElement('h3');
                 infoDiv.appendChild(h3);
+                h3.setAttribute('class', 'upload-time');
                 h3.innerHTML = list[i].CREATION;
                 var p = document.createElement('p');
                 p.setAttribute('id', 'id-' + list[i].ID);
@@ -191,32 +228,67 @@
                 var buttons = document.createElement('div');
                 card.appendChild(buttons);
                 buttons.setAttribute('class', 'buttons');
+                var likeDiv = document.createElement('div');
+                buttons.appendChild(likeDiv);
+                likeDiv.setAttribute('class', 'like-div');
                 var like = document.createElement('button');
-                buttons.appendChild(like);
+                likeDiv.appendChild(like);
                 like.setAttribute('class', 'like');
                 like.setAttribute('onclick', 'likeNews(this.id);'); // for FF
                 like.onclick = function() { likeNews(this.id); }; // for IE
                 like.setAttribute('id', 'like-' + list[i].ID);
-                like.innerHTML = 'Like';
+                like.setAttribute('type', 'submit');
+                var likeIcon = document.createElement('i');
+                like.appendChild(likeIcon);
+                likeIcon.setAttribute('class', 'far fa-thumbs-up');
                 var span = document.createElement('span');
                 like.appendChild(span);
                 span.innerHTML = list[i].LIKES;
+                var myLikes = document.createElement('div');
+                likeDiv.appendChild(myLikes);
+                myLikes.innerHTML = "Like";
+                var editDiv = document.createElement('div');
+                buttons.appendChild(editDiv);
+                editDiv.setAttribute('class', 'edit-div');
                 var edit = document.createElement('button');
-                buttons.appendChild(edit);
+                editDiv.appendChild(edit);
                 edit.setAttribute('class', 'edit');
                 edit.setAttribute('onclick', 'editNews(this.id);'); // for FF
                 edit.onclick = function() { editNews(this.id); }; // for IE
                 edit.setAttribute('id', list[i].ID);
-                edit.innerHTML = 'Edit';
+                var editIcon = document.createElement('i');
+                edit.appendChild(editIcon);
+                editIcon.setAttribute('class', 'far fa-edit');
+                var editTitle = document.createElement('div');
+                editDiv.appendChild(editTitle);
+                editTitle.innerHTML = "Edit";
+                var deleteDiv = document.createElement('div');
+                deleteDiv.setAttribute('class', 'delete-div');
+                buttons.appendChild(deleteDiv);
                 var deleted = document.createElement('button');
-                buttons.appendChild(deleted);
+                deleteDiv.appendChild(deleted);
                 deleted.setAttribute('class', 'delete');
                 deleted.setAttribute('onclick', 'deleteNews(this.id);'); // for FF
                 deleted.onclick = function() { deleteNews(this.id); }; // for IE
                 deleted.setAttribute('id', 'delete-' + list[i].ID);
-                deleted.innerHTML = 'Delete';
+                var deleteIcon = document.createElement('i');
+                deleted.appendChild(deleteIcon);
+                deleteIcon.setAttribute('class', 'far fa-trash-alt');
+                var deleteTitle = document.createElement('div');
+                deleteDiv.appendChild(deleteTitle);
+                deleteTitle.innerHTML = "Delete";
                 var inputDiv = document.createElement('div');
-                buttons.appendChild(inputDiv);
+                card.appendChild(inputDiv);
+                var spanComment = document.createElement('span');
+                inputDiv.appendChild(spanComment);
+                var link = document.createElement('a');
+                spanComment.appendChild(link);
+                link.setAttribute('class', 'number-of-comments');
+                link.setAttribute('href', '#');
+                link.setAttribute('id', 'comment-' + list[i].ID);
+                link.setAttribute('onclick', 'showComments(this.id);'); // for FF
+                link.onclick = function() { showComments(this.id); }; // for IE
+                link.innerHTML = list[i].COMMENTS + ' comments';
                 var comment = document.createElement('textarea');
                 inputDiv.appendChild(comment);
                 comment.setAttribute('rows', '1');
@@ -228,18 +300,10 @@
                 var comBtn = document.createElement('button');
                 inputDiv.appendChild(comBtn);
                 comBtn.innerHTML = "Submit";
+                comBtn.setAttribute('class', 'comment-button');
                 comBtn.setAttribute('onclick', 'submitComment(this.id);'); // for FF
                 comBtn.onclick = function() { submitComment(this.id); }; // for IE
                 comBtn.setAttribute('id', 'btn-id-' + list[i].ID);
-                var spanComment = document.createElement('span');
-                inputDiv.appendChild(spanComment);
-                var link = document.createElement('a');
-                spanComment.appendChild(link);
-                link.setAttribute('href', '#');
-                link.setAttribute('id', 'comment-' + list[i].ID);
-                link.setAttribute('onclick', 'showComments(this.id);'); // for FF
-                link.onclick = function() { showComments(this.id); }; // for IE
-                link.innerHTML = list[i].COMMENTS + ' comments';
                 i++;
             }
         } else {
@@ -263,7 +327,10 @@
         var response = http.responseText;
         console.log(response);
         if (http.status == 200) {
-            alert("Success!");
+            location.reload();
+            document.getElementById('news-entry').value = "";
+            modal.style.display = "none";
+            return false;
         } else {
             alert("Entry was not posted");
         }
@@ -281,6 +348,7 @@
         span.onclick = function() {
             modal.style.display = "none";
         }
+
         window.onclick = function(event) {
             if (event.target == modal) {
                 modal.style.display = "none";
@@ -304,7 +372,9 @@
             http.send(jsonString);
 
             if (http.status == 200) {
-                alert("Success!");
+                location.reload();
+                modal.style.display = "none";
+                return false;
             } else {
                 alert("Only news author can edit news!");
             }
@@ -340,8 +410,9 @@
             http.send(jsonString);
 
             if (http.status == 200) {
-                alert("Success!");
+                location.reload();
                 modal.style.display = "none";
+                return false;
             } else {
                 alert("Error");
                 modal.style.display = "none";
@@ -365,7 +436,8 @@
         http.send(jsonString);
 
         if (http.status == 200) {
-            alert("Success!");
+            location.reload();
+            return false;
         } else {
             alert("You already liked this post");
         }
@@ -391,15 +463,6 @@
             const list = JSON.parse(response);
             console.log(id);
             var i = 0;
-            // if (timesClicked == 1) {
-            //     var body = document.getElementById('all-coments');
-            //     body.style.display = "none";
-            //     timesClicked = 2;
-            // } else if (timesClicked == 2) {
-            //     var body = document.getElementById('all-coments');
-            //     body.style.display = "block";
-            //     timesClicked = 1;
-            // } else {
             var body = document.getElementById(id).parentElement.parentElement;
             var container = document.createElement('div');
             body.appendChild(container);
@@ -487,6 +550,10 @@
         } else {
             alert("Entry was not posted");
         }
+    }
+
+    function logout() {
+        window.location = "index.html";
     }
 
     // };
